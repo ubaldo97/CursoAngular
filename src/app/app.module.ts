d31fa2b7cc89a8788e8cca7549ecc813196491ce
ups <ups9703@gmail.com>
2021-02-09 12:15:15 -0600

    Creación de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EmpleadosComponent } from './empleados/empleados.component';
+import { EmpleadoComponent } from './empleado/empleado.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, EmpleadosComponent, EmpleadoComponent
   ],
   imports: [
     BrowserModule
